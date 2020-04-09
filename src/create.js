77328844be90e47b1b93271bed5fe01380a8c836
uoj <uoj@vixion360.com>
2020-04-09 12:22:50 +0200

    First commit

--- src/create.js
+++ src/create.js
@@ -1,9 +1,26 @@
 import { displayLog } from './utils';
+import { interval, timer } from 'rxjs';
 
 export default () => {
-    /** start coding */
 
+    // INTERVAL: Sirve para emitir secuencias de valores cada ciertos intervalo
+    const observable = interval(500);
 
+    const subscription = observable.subscribe(res => displayLog(res));
 
-    /** end coding */
+    // TIMER: Es el método equivalente a setTimeout de javascript para rxjs
+    timer(2000).subscribe(() => subscription.unsubscribe());
+
+    // Con timer también se pueden crear intervalos a partir del primer evento
+    // La primer vez se ejecuta a los 4 segundos y luego cada 100 milisegundos
+    const timerObservable = timer(4000, 100);
+
+    const subscription2 = timerObservable.subscribe(res => displayLog(`2 - ${res}`));
+
+    timer(6000).subscribe(() => subscription2.unsubscribe());
+
+    /* setTimeout(() => {
+        subscription.unsubscribe();
+    }, 10000); */
+    
 }
