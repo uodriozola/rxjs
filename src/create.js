9e127ebd00d1bb6b21e16295a03d2bbd6acd39db
uoj <uoj@vixion360.com>
2020-04-09 11:59:56 +0200

    Second commit

--- src/create.js
+++ src/create.js
@@ -4,8 +4,15 @@ import { from } from 'rxjs';
 export default () => {
     /** start coding */
     const myArray = [1, 2, 3, 4, 5];
+    const myString = "Hello world";
+    const myPromise = new Promise(res => {
+        setTimeout(() => {
+            res('Hello world');
+        }, 2000);
+    });
 
-    const observable = from(myArray);
+    // El operador from se utiliza para convertir Promises en Observables
+    const observable = from(myPromise);
 
     const subscription = observable.subscribe(res => displayLog(res));
 
