821392b39ab07d8742c0824d9b41b6e1b1c03743
uoj <uoj@vixion360.com>
2020-04-09 11:52:07 +0200

    First commit

--- src/create.js
+++ src/create.js
@@ -1,7 +1,13 @@
 import { displayLog } from './utils';
+import { from } from 'rxjs';
 
 export default () => {
     /** start coding */
+    const myArray = [1, 2, 3, 4, 5];
+
+    const observable = from(myArray);
+
+    const subscription = observable.subscribe(res => displayLog(res));
 
     /** end coding */
 }
