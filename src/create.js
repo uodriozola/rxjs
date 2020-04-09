ad357752e5b6b4b7eeb37b61d378504d00d07bef
uoj <uoj@vixion360.com>
2020-04-09 13:56:20 +0200

    First commit

--- src/create.js
+++ src/create.js
@@ -2,14 +2,38 @@ import { displayLog } from './utils';
 import { Observable } from 'rxjs';
 
 export default () => {
-    /** start coding */
-    const hello = Observable.create(function(observer) {
+    // Observable
+    const hello = Observable.create((observer) => {
         observer.next('Hello');
-        setTimeout(()=>{
+        setTimeout(() => {
             observer.next('World');
+            // Tras llamar al método complete() se dan por finalizadas las subscripciones (evitar memory leaks)
+            observer.complete();
         }, 2000);
     });
-    
-    const subscribe = hello.subscribe(evt => displayLog(evt));
+
+    // Observer
+    const observer = {
+        next: event => displayLog(event),
+        error: error => console.error('[ERR] - ', error),
+        complete: () => displayLog("[DONE]")
+    };
+
+    // Subscription al observer por defecto de rxjs
+    // Primer parámetro: next
+    // Segundo parámetro: error
+    // Tercer parámetro: complete
+    const subscribe1 = hello.subscribe(res =>
+        displayLog(res),
+        () => console.error('[ERR] - ', error),
+        () => displayLog("[DONE"));
+
+    //Subscription a nuestro observer custom
+    const subscribe2 = hello.subscribe(observer);
+    // Si me desuscribo del observer en este caso sobre mostrará "Hello"
+    // Porque ese primer evento es síncrono. El resto al ser asíncrono no llega a suceder
+    subscribe2.unsubscribe();
+
+
     /** end coding */
 }
