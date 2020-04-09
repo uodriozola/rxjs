46104b63a9a1355b5b0d45d2dfef3b476c3a7a2b
uoj <uoj@vixion360.com>
2020-04-09 12:36:21 +0200

    First commit

--- src/create.js
+++ src/create.js
@@ -1,9 +1,15 @@
 import { displayLog } from './utils';
+import { fromEvent } from 'rxjs';
 
 export default () => {
-    /** start coding */
+
+    const actionBtn = document.getElementById('action-btn');
+    // FROMEVENT: Es una función que asocia un observable a un event target concreto para emitir los eventos generados por este
+    const observable = fromEvent(actionBtn, 'click');
+
+    const subscription = observable.subscribe(res => displayLog(`click event at pos ${res.x},${res.y}`));
+
+    fromEvent(document, 'mousemove').subscribe(res => console.log(res));
 
 
-
-    /** end coding */
 }
