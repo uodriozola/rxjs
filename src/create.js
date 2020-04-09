b007c4578220c7f0b029cf9c8baa7bd9bbab1f3d
uoj <uoj@vixion360.com>
2020-04-09 12:09:42 +0200

    First commit

--- src/create.js
+++ src/create.js
@@ -1,9 +1,12 @@
 import { displayLog } from './utils';
+import { of, range } from 'rxjs';
 
 export default () => {
-    /** start coding */
+    // OF: Sirve para crear secuencias de observables a partir de prácticamente cualquier cosa
+    const source = of(1, 2, 3, 4, 5, 6);
+    const source2 = of([1, 2, 3], 'Hello world', { foo: 'bar'}, function sayHello() { return 'Hi' });
+    // RANGE: Sirve para crear un observable a partir de una secuencia ordenada de números
+    const source3 = range(3, 10);
 
-
-
-    /** end coding */
+    const subscription = source3.subscribe(res => displayLog(res));
 }
