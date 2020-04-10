b1638059a6c1414a53e39dce90e6121781753dd9
uoj <uoj@vixion360.com>
2020-04-10 16:31:16 +0200

    First commit

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,18 +1,18 @@
 import { displayLog } from './utils';
 import { fromEvent } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 
 export default () => {
-    /** start coding */
     const grid = document.getElementById('grid');
+    // tap: Se utiliza para modificar algo (o generar acciones) en base al flujo de datos fuera de este
     const click$ = fromEvent(grid, 'click').pipe(
+        tap(res => console.log('Before: ', res)),
         map(val => [ 
             Math.floor(val.offsetX/50), 
             Math.floor(val.offsetY/50)
-        ])
+        ]),
+        tap(res => console.log(`After: ${res}`))
     );
 
     const subscription = click$.subscribe(data => displayLog(data));
-
-    /** end coding */
 }
