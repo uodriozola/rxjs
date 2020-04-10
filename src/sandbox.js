0d5763833d227f8e4d8a4021f3a36542d7b32eb8
uoj <uoj@vixion360.com>
2020-04-10 18:37:30 +0200

    First commit

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,19 +1,23 @@
 import { updateDisplay } from './utils';
 import { fromEvent } from 'rxjs';
-import { map, tap } from 'rxjs/operators';
+import { map, tap, pairwise } from 'rxjs/operators';
 
 export default () => {
-    /** start coding */
     const progressBar = document.getElementById('progress-bar');
     const docElement = document.documentElement;
     const updateProgressBar = (percentage) => {
         progressBar.style.width = `${percentage}%`;
     }
 
+    // pairwise: Se encarga de emitir los eventos en parejas de datos consecutivos
+
     //observable that returns scroll (from top) on scroll events
     const scroll$ = fromEvent(document, 'scroll').pipe(
         map(() => docElement.scrollTop),
-        tap(evt => console.log("[scroll]: ", evt))
+        tap(evt => console.log("[scroll]: ", evt)),
+        pairwise(),
+        tap(([previous, current]) => updateDisplay(current > previous ? 'DESC' : 'ASC')),
+        map(([previous, current]) => current)
     );
 
     //observable that returns the amount of page scroll progress
@@ -26,6 +30,4 @@ export default () => {
 
     //subscribe to scroll progress to paint a progress bar
     const subscription = scrollProgress$.subscribe(updateProgressBar);
-
-    /** end coding */
 }
