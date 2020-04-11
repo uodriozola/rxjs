21f577d29f5d3b574bb470e0b6c0b3f4939a6b80
uoj <uoj@vixion360.com>
2020-04-11 18:01:41 +0200

    First commit

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,9 +1,8 @@
 import { updateDisplay } from './utils';
 import { fromEvent } from 'rxjs';
-import { map, tap } from 'rxjs/operators';
+import { map, tap, sampleTime, auditTime, throttleTime } from 'rxjs/operators';
 
 export default () => {
-    /** start coding */
     
     const progressBar = document.getElementById('progress-bar');
     const docElement = document.documentElement;
@@ -13,14 +12,35 @@ export default () => {
         progressBar.style.width = `${percentage}%`;
     }
 
-    //observable that returns scroll (from top) on scroll events
-    const scroll$ = fromEvent(document, 'scroll').pipe(
+    // sampleTime: Operador que emite el valor más reciente de un flujo de datos cada cierto tiempo
+    // siempre y cuando el flujo de datos haya emitido algún valor en el intervalo
+    const scrollSample$ = fromEvent(document, 'scroll').pipe(
+        tap(evt => console.log(evt)),
+        sampleTime(50),
+        map(() => docElement.scrollTop),
+        tap(evt => console.log("[scroll]: ", evt))
+    );
+
+    // auditTime: Espera a detectar un evento, y en ese momento crea una ventana temporal del tamaño indicado
+    // Una vez pasado ese tiempo emite el valor del último evento detectado en ese intervalo
+    const scrollAudit$ = fromEvent(document, 'scroll').pipe(
+        tap(evt => console.log(evt)),
+        auditTime(50),
+        map(() => docElement.scrollTop),
+        tap(evt => console.log("[scroll]: ", evt))
+    );
+
+    // throttleTime: Cuando detecta un evento lo emite y acto seguido deja de escuchar el stream durante la ventana temporal que se le indique
+    // Cuando acaba ese intervalo de tiempo se vuelve a quedar a la espera para el siguiente nuevo evento
+    const scrollThrottle$ = fromEvent(document, 'scroll').pipe(
+        tap(evt => console.log(evt)),
+        throttleTime(50),
         map(() => docElement.scrollTop),
         tap(evt => console.log("[scroll]: ", evt))
     );
 
     //observable that returns the amount of page scroll progress
-    const scrollProgress$ = scroll$.pipe(
+    const scrollProgress$ = scrollThrottle$.pipe(
         map(evt => {
             const docHeight = docElement.scrollHeight - docElement.clientHeight;
             return (evt / docHeight) * 100;
