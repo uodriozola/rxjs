f7b3861381182bf095c59add74030c35d78eb795
uoj <uoj@vixion360.com>
2020-04-11 18:37:35 +0200

    First commit

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,9 +1,17 @@
 import { updateDisplay, displayLog } from './utils';
+import { fromEvent } from 'rxjs';
+import { map, debounceTime } from 'rxjs/operators';
 
 export default () => {
-    /** start coding */
     
     const inputBox = document.getElementById('input-box');
 
-    /** end coding */
+    // debounceTime: Cuando se recibe un evento se inicia una espera en la que se escuchan valores y al acabar la espera se emite el valor más actual
+    // La diferencia con auditTime es que debounceTime además reinica la espera con cada nuevo valor recibido
+    const inputSrc$ = fromEvent(inputBox, 'input').pipe(
+        map(res => res.target.value),
+        debounceTime(1000)
+    );
+
+    const suscription = inputSrc$.subscribe(res => displayLog(res));
 }
