adb9a69e16bfd2c235c7aa06508aad33ce57753b
uoj <uoj@vixion360.com>
2020-04-12 12:50:34 +0200

    First commit

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,20 +1,34 @@
 import { updateDisplay, displayLog } from './utils';
 import { api } from './api';
 import { fromEvent } from 'rxjs';
-import { map, scan, tap, mergeMap } from 'rxjs/operators';
+import { map, scan, tap, mergeMap, switchMap, concatMap } from 'rxjs/operators';
 
 export default () => {
-    /** start coding */
-    
     const button = document.getElementById('btn');
 
     /** get comments on button click */
-    fromEvent(button, 'click').pipe(
+    /* fromEvent(button, 'click').pipe(
         scan((acc, evt) => acc + 1, 0),            
         mergeMap(id => api.getComment(id)),
         map(JSON.stringify),
         tap(console.log),
-    ).subscribe(displayLog);
+    ).subscribe(displayLog); */
 
-    /** end coding */
+    // switchMap: Cuando recibe un evento genera un observable interno y se suscribe a él para pasarle sus eventos al observable externo
+    // Cuando recibe un nuevo evento externo cancela la suscripción del evento interno anterior antes de suscribirse a un nuevo evento interno
+    /* fromEvent(button, 'click').pipe(
+        scan((acc, evt) => acc + 1, 0),            
+        switchMap(id => api.getComment(id)),
+        map(JSON.stringify),
+        tap(console.log),
+    ).subscribe(displayLog); */
+
+    // concatMap: Se suscribe a los observables internos de forma ordenada
+    // Hasta que no se completen los eventos del primer observable interno no se suscribirá al segundo, y así sucesivamente
+    fromEvent(button, 'click').pipe(
+        scan((acc, evt) => acc + 1, 0),            
+        concatMap(id => api.getComment(id)),
+        map(JSON.stringify),
+        tap(console.log),
+    ).subscribe(displayLog);
 }
