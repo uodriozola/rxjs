76ca71fb6ea40201ea4c7c60d45508946734db3d
uoj <uoj@vixion360.com>
2020-04-12 11:57:02 +0200

    First commit

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,7 +1,7 @@
 import { updateDisplay, displayLog } from './utils';
 
-import { fromEvent } from 'rxjs';
-import { map, debounceTime } from 'rxjs/operators';
+import { fromEvent, combineLatest } from 'rxjs';
+import { map, debounceTime, withLatestFrom} from 'rxjs/operators';
 
 export default () => {
     /** start coding */
@@ -22,10 +22,20 @@ export default () => {
         debounceTime(400),
         map(evt => evt.target.value)
     );
+
     const submitButton$ = fromEvent(form.btn, 'click');
 
+    // combineLatest: Junta varios streams de entrada, y cada vez que recibe un evento emite un array con el último valor de cada entrada
+    const formDataCombine$ = combineLatest(formName$, formEmail$, formNumber$);
 
-    
+    // withLatestFrom: Similar a combineLatest, pero sólo emite datos cuando su stream de origen los emite
+        const formDataLatest$ = submitButton$.pipe(
+            withLatestFrom(formName$, formEmail$, formNumber$),
+            map(res => {
+                const [click, ...formData] = res;
+                return formData
+            })
+        );
 
-    /** end coding */
+    formDataLatest$.subscribe(res => displayLog(res));
 }
