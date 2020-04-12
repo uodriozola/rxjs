75f5fd9d75c2cc5780b6883f26ccdc926bdbb25c
uoj <uoj@vixion360.com>
2020-04-12 11:43:27 +0200

    First commit

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,6 +1,6 @@
 import { updateDisplay, displayLog } from './utils';
 import { api } from './api';
-import { merge, fromEvent } from 'rxjs';
+import { merge, fromEvent, concat, forkJoin } from 'rxjs';
 import { map, endWith } from 'rxjs/operators';
 
 export default () => {
@@ -17,8 +17,25 @@ export default () => {
         const comment4$ = api.getComment(4);
 
         //subscribe to all the observables to get and display comments
-        merge(comment1$, comment2$, comment3$, comment4$).pipe(
+        /* merge(comment1$, comment2$, comment3$, comment4$).pipe(
             map(({id, comment}) => `#${id} - ${comment}`),
+            endWith('--------//--------')
+        ).subscribe(data =>{
+            displayLog(data);
+        }) */
+
+        // concat: Se utiliza para emitir los eventos de un conjunto de Observables en orden
+        /* concat(comment1$, comment2$, comment3$, comment4$).pipe(
+            map(({id, comment}) => `#${id} - ${comment}`),
+            endWith('--------//--------')
+        ).subscribe(data =>{
+            displayLog(data);
+        }) */
+
+        // forkJoin: Recibe varios observables de entrada, se espera a que se completen todos,
+        // y entonces emite un array ordenado con el último valor de cada flujo de entrada
+        forkJoin(comment1$, comment2$, comment3$, comment4$).pipe(
+            map(JSON.stringify),
             endWith('--------//--------')
         ).subscribe(data =>{
             displayLog(data);
