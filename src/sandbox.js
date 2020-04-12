b0eeb604f89f57100154ea062cedd560be3c51e9
uoj <uoj@vixion360.com>
2020-04-12 12:57:52 +0200

    First commit

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,20 +1,21 @@
 import { updateDisplay, displayLog } from './utils';
 import { api } from './api';
-import { fromEvent } from 'rxjs';
-import { map, scan, tap, concatMap } from 'rxjs/operators';
+import { fromEvent, from } from 'rxjs';
+import { map, scan, tap, concatMap, mergeMap } from 'rxjs/operators';
 
 export default () => {
-    /** start coding */
-    
+
     const button = document.getElementById('btn');
 
     /** get comments on button click */
     fromEvent(button, 'click').pipe(
-        scan((acc, evt) => acc + 1, 0),            
+        scan((acc, evt) => acc + 1, 0), 
+        // recibimos un evento con un array de valores procecentes de un observable          
         concatMap(page => api.getCommentsList(page)),
+        // transformamos el evento anterior en una secuencia de eventos donde hay un evento por cada elemento del array
+        mergeMap(comments => from(comments)),
         map(JSON.stringify),
         tap(console.log),
     ).subscribe(displayLog);
 
-    /** end coding */
 }
