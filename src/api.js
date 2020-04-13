3b7d1ec805142914edb5f5957df5520284a24b96
uoj <uoj@vixion360.com>
2020-04-13 10:44:56 +0200

    First commit

--- src/api.js
+++ src/api.js
@@ -1,10 +1,21 @@
-import { timer } from 'rxjs';
-import { mapTo } from 'rxjs/operators';
+import { timer, throwError, of } from 'rxjs';
+import { mapTo, mergeMap } from 'rxjs/operators';
 
 export class api{
+
+    // throwError: Crea un observable que tal como se crea lanza un error
+    // Cuando la suscripción captura un error se cierra
+    // Es útil para lanzar errores cuando lo combinas con otros observables
     static getComment(id){
         return timer(Math.random()*1000).pipe(
-            mapTo({id:id, comment:`comment number ${id}`})
+            mergeMap(() => {
+                const isError = Math.random() > 0.6 ? true : false;
+                if(isError) {
+                    return throwError(new Error('Request Timeout'));
+                } else {
+                    return of({id:id, comment:`comment number ${id}`})
+                }
+            })
         );
     }
 
--- src/sandbox.js
+++ src/sandbox.js
@@ -1,20 +1,23 @@
 import { updateDisplay, displayLog } from './utils';
 import { api } from './api';
 import { fromEvent } from 'rxjs';
-import { map, scan, tap, concatMap } from 'rxjs/operators';
+import { map, scan, tap, concatMap, catchError, retry } from 'rxjs/operators';
 
 export default () => {
-    /** start coding */
-    
+
     const button = document.getElementById('btn');
 
-    /** get comments on button click */
     fromEvent(button, 'click').pipe(
         scan((acc, evt) => acc + 1, 0),            
-        concatMap(id => api.getComment(id)),
+        concatMap(id => api.getComment(id).pipe(
+            // catchError: Permite intervenir el error a nivel de observable, es decir, antes de que se emita hacia la suscripción y la cierre
+            // Además del error recibe un objeto con el observable original
+            // catchError((err, src$) => { console.log('catch!'); return src$ }),
+            // retry: Permite capturar una excepción en el flujo de datos y reintentar su ejecución un número limitado de veces
+            retry(3)
+        )),
         map(JSON.stringify),
         tap(console.log),
-    ).subscribe(displayLog);
-
-    /** end coding */
+        // Desde la suscripción capturamos el error throwError lanzado desde api.js
+    ).subscribe(displayLog, err => console.log('Error: ', err.message));
 }
